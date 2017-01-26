0898ad8b4c86f05424ad4ea60628484e4742b3dc
kottov <kottov@gmail.com>
2017-01-26 19:17:35 +0200

    Update todo route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectId } = require('mongoose').Types;
@@ -54,6 +55,27 @@ app.delete('/todos/:id', (req, res) => {
             res.send({todo});
         })
         .catch((e) => res.status(400).send());
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectId.isValid(req.params.id)) res.status(404).send();
+    
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+        .then((todo) => {
+            if(!todo) res.status(404).send();
+            res.send({todo});
+        })
+        .catch((e) => res.status(400).send(e));
 });
 
 app.listen(port, () => console.log(`Web server started on port ${port}`));
