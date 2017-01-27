fb9ffaf4dceba43f163a42aae514c0d30b269c18
kottov <kottov@gmail.com>
2017-01-27 23:31:48 +0200

    Remove SALT_DEEP test route

--- server/server.js
+++ server/server.js
@@ -98,10 +98,6 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
-app.get('/salt', (req, res) => {
-    res.send(process.env.SALT_DEEP);
-});
-
 app.listen(port, () => console.log(`Web server started on port ${port}`));
 
 module.exports = { app };
